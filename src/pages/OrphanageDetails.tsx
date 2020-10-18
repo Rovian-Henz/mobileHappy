279dda9351443a18f2ff737d2177840b9672da0b
Rovian <rovian.henz@gmail.com>
2020-10-18 18:12:24 -0300

    Add OrphanageMap page

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+export default function OrphanageDetails() {
+    return <View />;
+}
